31f44b1107a9525483c4fc356c61e5726880844a
Ronit <malvironit1103@gmail.com>
2024-08-27 17:56:27 +0530

    login page

---
+++ web-app/client/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+import Login from './pages/Login'
+import {Routes, Route, Navigate, Outlet, useLocation} from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Tasks from './pages/Tasks'
+import Users from './pages/Users'
+import Trash from './pages/Trash'
+import TaskDetails from './pages/TaskDetails'
+import { Toaster } from 'sonner'
+
+
+function Layout(){
+  const user=""
+  const location=useLocation()
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screem bg-white sticky top-0 hidden md:block'>
+      {/*<Sidebar/> */}
+      </div>
+      {/*<MobileSidebar/> */}
+
+      <div className='flex-1 overflow-y-auto'>
+        {/*<Navbar/> */}
+        <div className='p-4 2xl:px-10'>
+          <Outlet/>
+        </div>
+      </div>
+    </div>
+  ):(
+    <Navigate to="/log-in" state={{from:location}} replace/>
+  )
+}
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path='/' element={<Navigate to="/dashboard"/>}/>
+          <Route path='/dashboard >' element={<Dashboard/>}/>
+          <Route path='/tasks >' element={<Tasks/>}/>
+          <Route path='/completed/:status >' element={<Tasks/>}/>
+          <Route path='/in-progress/:status >' element={<Tasks/>}/>
+          <Route path='/todo/:status >' element={<Tasks/>}/>
+          <Route path='/team >' element={<Users/>}/>
+          <Route path='/trashed >' element={<Trash/>}/>
+          <Route path='/task/:id >' element={<TaskDetails/>}/>
+        </Route>
+
+        <Route path='/log-in' element={<Login/>}/>
+      </Routes>
+
+      <Toaster richColors/>
+
+    </main>
+  )
+}
+
+export default App
